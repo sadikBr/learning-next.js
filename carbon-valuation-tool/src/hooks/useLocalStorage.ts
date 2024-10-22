1dd7fd0e6a4e50d68ab5c34936be4b9c655c4171
SADIK Brahim <62760511+sadikBr@users.noreply.github.com>
2024-10-22 22:53:59 +0100

    updates

--- carbon-valuation-tool/src/hooks/useLocalStorage.ts
+++ carbon-valuation-tool/src/hooks/useLocalStorage.ts
@@ -4,16 +4,16 @@ type THEME = 'dark' | 'light';
 
 export default function useLocalStorage(key: string) {
   const [item, setItem] = useState<THEME>(() => {
-    const item = localStorage.getItem(key);
+    const item = window.localStorage.getItem(key);
     if (item) {
       return item as THEME;
     }
-    localStorage.setItem(key, 'dark');
+    window.localStorage.setItem(key, 'dark');
     return 'dark';
   });
 
   useEffect(() => {
-    localStorage.setItem(key, item);
+    window.localStorage.setItem(key, item);
   }, [item, key]);
 
   return {
